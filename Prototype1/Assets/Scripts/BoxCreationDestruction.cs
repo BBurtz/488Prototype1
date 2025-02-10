1b5b580b469f16809f284c733ea042fd968bcc68
Karkaranos <karkaranosyt@gmail.com>
2025-02-10 10:28:32 -0600

    fixed one bug?

--- Prototype1/Assets/Scripts/BoxCreationDestruction.cs
+++ Prototype1/Assets/Scripts/BoxCreationDestruction.cs
@@ -15,31 +15,40 @@ public class BoxCreationDestruction : MonoBehaviour
     public  GameObject originalBox;
     [SerializeField] private Material origM;
     [SerializeField] private Material deactiveM;
+    [SerializeField] bool isActive;
 
-    /*
+    
     public void destroyBox()
     {
+        PlayerMovement pm = FindFirstObjectByType<PlayerMovement>();
         if (originalBox.activeInHierarchy && !linkedBox.activeInHierarchy)
         {
             linkedBox.SetActive(true);
             originalBox.SetActive(false);
+            pm.CDInRange.Remove(originalBox.GetComponent<BoxCreationDestruction>());
+            pm.BoxesInRange.Remove(originalBox.GetComponent<BoxBehavior>());
+
             Debug.Log("Does destroying original work?");
         }
         else if (!originalBox.activeInHierarchy && linkedBox.activeInHierarchy)
         {
             originalBox.SetActive(true);
             linkedBox.SetActive(false);
+            pm.CDInRange.Remove(linkedBox.GetComponent<BoxCreationDestruction>());
+            pm.BoxesInRange.Remove(linkedBox.GetComponent<BoxBehavior>());
+
         }
         else
         {
             Debug.LogWarning("Warning! Make sure one of the boxes is inactive in the hierarchy");
         }
     }
-    */
+    
 
-    public void destroyBox()
+    /*public void destroyBox()
     {
-        if(originalBox.GetComponent<MeshRenderer>().material == origM)
+        isActive = !isActive;
+        if(!isActive)
         {
             originalBox.GetComponent<MeshRenderer>().material = deactiveM;
             linkedBox.GetComponent<MeshRenderer>().material = origM;
@@ -49,12 +58,12 @@ public class BoxCreationDestruction : MonoBehaviour
             linkedBox.GetComponent<MeshRenderer>().material = deactiveM;
             originalBox.GetComponent<MeshRenderer>().material = origM;
         }
-    }
+    }*/
 
     private void OnTriggerEnter(Collider other)
     {
         //Check if the object has player movement
-        if (other.GetComponent<PlayerMovement>() != null)
+        if (other.GetComponent<PlayerMovement>() != null && !other.gameObject.GetComponent<PlayerMovement>().PushToMoveBlocks)
         {
                 //Add the current box to the player's box list
                 other.GetComponent<PlayerMovement>().CDInRange.Add(this);
