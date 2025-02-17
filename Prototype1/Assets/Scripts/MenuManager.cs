8bb84b692f5b16270cd138aaeecc9f4cf2f8a63e
TheGoat2302 <126194652+TheGoat2302@users.noreply.github.com>
2025-02-17 11:33:38 -0600

    MainMenu

--- Prototype1/Assets/Scripts/MenuManager.cs
+++ Prototype1/Assets/Scripts/MenuManager.cs
@@ -3,18 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public void LoadScene(int Scene)
+    public void Play()
     {
-        SceneManager.LoadScene(Scene);
+        SceneManager.LoadScene(1);
     }
 
-    public void Close()
+    public void Quit()
     {
         Application.Quit();
+        print("WORKS");
     }
 
-    public void MainMenu()
+    public void Menu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void LevelButtonPressed(int level)
+    {
+        SceneManager.LoadScene(level);
     }
 }
