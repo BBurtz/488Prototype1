72578c57d84da46705e36512e635595aca9e09eb
BBurtz <117851173+BBurtz@users.noreply.github.com>
2025-02-17 11:48:28 -0600

    Shitty Pause

--- Prototype1/Assets/Scripts/MenuManager.cs
+++ Prototype1/Assets/Scripts/MenuManager.cs
@@ -1,8 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject PauseMenu;
+
+    public void UnPause()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     public void LoadScene(int Scene)
     {
         SceneManager.LoadScene(Scene);
@@ -15,6 +25,7 @@ public class MenuManager : MonoBehaviour
 
     public void MainMenu()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(0);
     }
 }
